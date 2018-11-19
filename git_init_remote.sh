747b2efae27fccf91d9c89f5e113a9a24503b453
Alexander Fall <4alexfall@gmail.com>
2018-11-18 23:33:15 -0500

    better support for paths, '.', './', etc.

--- git_init_remote.sh
+++ git_init_remote.sh
@@ -3,10 +3,9 @@
 # run after 'git init' 'git add' and 'git commit -m "first commit"'
 # after thes commands above the local repo is setup
 # now to duplicate it to the server and setup the remote
-
-if [ $# -eq 0 ]
+if [ ! $# -eq 1 ]
   then
-    echo "No arguments supplied"
+    echo "Expects one argument, directory of git repo"
     exit 1
 fi
 
@@ -16,8 +15,14 @@ if [ ! -d "$1" ]
     exit 1
 fi
 
+if [ ! -d "$1/.git" ]
+  then
+    echo "$1 does not contain '.git/' directory"
+    exit 1
+fi
+
 GIT_URL="git@vcs.fallalex.com:/srv/git/"
-REPO_PATH="$(realpath $1)"
+REPO_PATH="$(greadlink -f $1)"
 REPO_NAME="$(basename $REPO_PATH).git"
 BARE_REPO_PATH="$REPO_PATH/$REPO_NAME"
 
